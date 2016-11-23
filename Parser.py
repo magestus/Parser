c9fb0f0a2020f52a2e60066b8c61515576ee73dd
magestus <magestus@yandex.ru>
2016-11-23 16:47:49 +0500

    1

--- Parser.py
+++ Parser.py
@@ -5,15 +5,19 @@ from bs4 import BeautifulSoup
 
 def get_html(url):
     response = urllib.request.urlopen(url)
-    return str(response.read())
+    return response.read()
 
 def parse(html):
     soup = BeautifulSoup(html, 'html.parser')
     table = soup.find('div', {"class": "Search-list"})
-    print(table)
+    
+    for row in table.find_all('div', {'Search-result-item'}):
+        cols = row.find_all('a')    
+        print(cols)
+    
 
 def main():
-    parse(get_html('https://www.fabrikant.ru/trades/procedure/search/'))
+    parse(get_html('https://fabrikant.ru/trades/procedure/search/?type=1&query=&procedure_stage=2&price_from=&price_to=&currency=0&date_type=date_publication&date_from=&date_to=&ensure=all&count_on_page=10&order_by=default&order_direction=1'))
 
 if __name__ == "__main__":
     main()
